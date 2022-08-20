e957e667920d5d15a185648ffdced3e1663435b4
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-20 21:24:32 +0200

    Remove files

--- src/helpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.calculateDistance = void 0;
-var calculateDistance = function (objectA, objectB) {
-    return objectA - objectB;
-};
-exports.calculateDistance = calculateDistance;
