7b254ac382f956fdb5ae416177270c3236575646
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-20 21:26:01 +0200

    Create helper function for generate random numbers and add text

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,28 @@
+import { TextStyle, Text } from "pixi.js";
+
 export const calculateDistance = (objectA: number, objectB: number) => {
   return objectA - objectB;
+};
+
+export const addTitle = (): Text => {
+  const style = new TextStyle({
+    fontFamily: "Montserrat",
+    fontSize: 45,
+    fill: 0xdddddd,
+    align: "center",
+  });
+  const text = new Text("FOOD MOOD", style);
+  text.anchor.x = 0.5;
+  text.position.x = window.innerWidth / 2;
+  text.position.y = 10;
+  return text;
+};
+
+export const getRandomNumber = (range: number): number => {
+  return Math.floor(Math.random() * range);
+};
+
+export const getRandomXPosition = (width: number): number => {
+  const margin = (window.innerWidth - width) / 2;
+  return margin + Math.random() * width;
 };
