3214394b2d0f88ee2c42f73b35012b9fcf2ab823
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-20 21:28:35 +0200

    Add App class and title to main script

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import { calculateDistance } from "./helpers.js";
-import * as PIXI from "../node_modules/pixi.js";
+import { AppManager } from "./App";
+import { addTitle } from "./helpers";
 
-class Game {
-  points: number;
-  constructor(points = 100) {
-    this.points = points;
-  }
-}
-const app = new PIXI.Application();
-const game = new Game();
-console.log(calculateDistance(10, 2), app, "kefrrsccc");
+const app = new AppManager();
+
+const text = addTitle();
+app.app.stage.addChild(text);
