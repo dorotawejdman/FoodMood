8f7f59da647075094df00d394192778e648e2e8f
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-21 17:40:06 +0200

    Add startHP to gameSettings

--- src/models/GameSettings.ts
+++ src/models/GameSettings.ts
@@ -1,8 +1,10 @@
 export class GameSettings {
   foodDropFreq: number;
   dropsPerLevel: number;
-  constructor(foodDropFreq: number = 100, dropsPerLevel: number = 10) {
+  startHP: number;
+  constructor(foodDropFreq: number = 100, dropsPerLevel: number = 20, startHP = 10) {
     this.foodDropFreq = foodDropFreq;
     this.dropsPerLevel = dropsPerLevel;
+    this.startHP = startHP;
   }
 }
