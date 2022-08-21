4ac4bd4d097efb2e295f9f2ac57a157a4e7a8f80
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-21 13:40:09 +0200

    Create food class

---
+++ src/Food.ts
@@ -0,0 +1,20 @@
+import { Sprite, Texture } from "pixi.js";
+import { getRandomNumber, getRandomXPosition } from "./helpers";
+import { Position } from "./models/Position";
+import * as PIXI from "pixi.js";
+
+export class Food extends Sprite {
+  constructor(foodTextures: Texture<PIXI.Resource>[], level: number) {
+    const textureId = getRandomNumber(4);
+    const foodStartPosition = { x: getRandomXPosition(level * 100), y: 100 };
+    super(foodTextures[textureId]);
+    this.anchor.set(0.5);
+    this.position.x = foodStartPosition.x;
+    this.position.y = foodStartPosition.y;
+    this.scale.set(2, 2);
+  }
+
+  move(velocity: number = 1) {
+    this.position.y += velocity;
+  }
+}
