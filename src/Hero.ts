ab867f3ec4275f2bf14dc37cd95f63abafc96f92
dorotawejdman <dorota.wejdman@gmail.com>
2022-08-21 17:39:10 +0200

    Move hp to game class

--- src/Hero.ts
+++ src/Hero.ts
@@ -3,24 +3,18 @@ import { Position } from "./models/Position";
 
 export class Hero extends Sprite {
   name: string;
-  hp: number;
-  constructor(position: Position, texture: any, name: string, hp: number = 100) {
+  constructor(position: Position, texture: any, name: string) {
     super(texture);
     this.anchor.set(0.5);
     this.name = name;
-    this.hp = hp;
     this.position.x = position.x;
     this.position.y = position.y;
   }
 
   move(velocity: number) {
-    const newXPosition = (this.position.x += velocity);
-    if (newXPosition < window.innerWidth && newXPosition > 0) {
+    const newXPosition = this.position.x + velocity;
+    if (newXPosition < window.innerWidth - 20 && newXPosition > 20) {
       this.position.x = newXPosition;
     }
-  }
-
-  removeHP() {
-    this.hp -= 1;
   }
 }
